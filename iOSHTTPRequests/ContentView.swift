d55550ba615ad17062c3c915c7d4a40e32fa56ad
Emre Dogan <emre.dogan@shortcut.no>
2023-01-22 17:10:03 +0100

    Initial Commit

---
+++ iOSHTTPRequests/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOSHTTPRequests
+//
+//  Created by Emre Dogan on 22/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSHTTPRequests/iOSHTTPRequestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSHTTPRequestsApp.swift
+//  iOSHTTPRequests
+//
+//  Created by Emre Dogan on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSHTTPRequestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
